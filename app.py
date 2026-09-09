7acdb6581af46dd6bd3f46f4ad68591e482a8bf8
0348861186 <tanlhu.lkt@gmail.com>
2026-09-09 09:36:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ if st.button("🚀 Chạy phân tích chấm công với Gemini AI", type="prima
                 """
 
                 response = client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model='gemini-1.5-flash',
                     contents=prompt,
                 )
                 
